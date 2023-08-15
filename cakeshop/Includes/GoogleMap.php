868d398ceeb5b2f53526176feb6457e161fa03bc
KrishnaDev Royal <125939809+KrishnaDevRoyal@users.noreply.github.com>
2023-08-15 12:08:45 +0530

    Add files via upload

---
+++ cakeshop/Includes/GoogleMap.php
@@ -0,0 +1,11 @@
+<?php include "./AdditionalPHP/checkAccess.php"; ?>
+
+<div class="map">
+    <div class="subtitle">
+        <h2>LOCATE US</h2>
+    </div>
+
+    <div class="google-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3838.961476242452!2d78.0364733!3d15.8059852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb5dd84f8302457%3A0xfdfd2924af53f1c6!2sThe%20Best%20Cakes%20(Dolphin%20Bakery)!5e0!3m2!1sen!2sin!4v1692003299084!5m2!1sen!2sin" width="100%"" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">></iframe>
+    </div>
+</div>
